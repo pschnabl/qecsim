fb22b4c144f85097716621e8e29e5ff67410c414
pschnabl <pjschnabl@gmail.com>
2024-03-05 10:06:57 +0100

    Add RotatedPlanarMWPMDecoder to rotatedplanar module

--- src/qecsim/models/rotatedplanar/__init__.py
+++ src/qecsim/models/rotatedplanar/__init__.py
@@ -8,3 +8,4 @@ from ._rotatedplanarcode import RotatedPlanarCode  # noqa: F401
 from ._rotatedplanarmpsdecoder import RotatedPlanarMPSDecoder  # noqa: F401
 from ._rotatedplanarrmpsdecoder import RotatedPlanarRMPSDecoder  # noqa: F401
 from ._rotatedplanarsmwpmdecoder import RotatedPlanarSMWPMDecoder  # noqa: F401
+from ._rotatedplanarmwpmdecoder import RotatedPlanarMWPMDecoder  # noqa: F401
